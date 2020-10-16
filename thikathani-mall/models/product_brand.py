5f512c85db7b9e91e33c41f27eecf69d6e1d9782
Kyssnar <ailton.salg@gmail.com>
2020-10-16 13:24:45 -0500

    [TEST] Product brand

--- thikathani-mall/models/product_brand.py
+++ thikathani-mall/models/product_brand.py
@@ -4,6 +4,5 @@ from odoo import models, fields, api
 
 class ProductBrand(models.Model):
     _name = 'product.brand'
-    
-#     _name = 'product_ilo.product_ilo'
-#     _description = 'product_ilo.product_ilo'
+    code = fields.Char('Codigo')
+    name = fields.Char('Nombre')
