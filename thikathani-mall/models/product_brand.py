95498ff8f5d1c718fc833f7762f0d02d860fc0b8
Kyssnar <ailton.salg@gmail.com>
2020-10-16 22:53:09 -0500

    [ADD] more info page

--- thikathani-mall/models/product_brand.py
+++ thikathani-mall/models/product_brand.py
@@ -5,4 +5,4 @@ from odoo import models, fields, api
 class ProductBrand(models.Model):
     _name = 'product.brand'
     code = fields.Char('Codigo')
-    name = fields.Char('Nombre')
+    brand_name = fields.Char('Nombre')
