d4d7369ad01ff12d78d336417eea784c38f6d80c
Kyssnar <ailton.salg@gmail.com>
2020-10-17 18:40:12 -0500

    [FIX] view product

--- thikathani-mall/models/product_type.py
+++ thikathani-mall/models/product_type.py
@@ -2,7 +2,6 @@ from odoo import models, fields, api
 
 class ProductType(models.Model):
     _name = 'product.type'
-    _description = 'Tipo de producto'
-    name = fields.Char(string='Nombre', required=True)
+    name = fields.Char('Nombre', required=True)
     type_image = fields.Binary()
     products_ids = fields.One2many('product.template', 'type_id')
