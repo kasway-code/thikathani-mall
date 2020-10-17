233fd8f97779246b8d6f6f8b3fb94be1c6dd84d3
Kyssnar <ailton.salg@gmail.com>
2020-10-17 18:20:08 -0500

    [FIX] view product

--- thikathani-mall/models/product_brand.py
+++ thikathani-mall/models/product_brand.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
-'''
-class ProductBrand(models.Model):
-    _name = 'product.brand'
-    code = fields.Char('Codigo')
-    brand_name = fields.Char('Nombre')
-'''
 
 class BrandProduct(models.Model):
     _name = 'product.brand'
--- thikathani-mall/models/product_template.py
+++ thikathani-mall/models/product_template.py
@@ -9,6 +9,7 @@ class ProductTemplate(models.Model):
     descripcion_usos = fields.Text('Modos de uso')
     
     brand_id = fields.Many2one('product.brand',string='Marca')
+    ttype_id = fields.Many2one('product.type',string='Tipo')
     sku = fields.Char(
         string='SKU',
     )
