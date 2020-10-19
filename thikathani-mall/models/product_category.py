aa6a8f47ab67e468cf0e2f9c5aefa489035e1745
Kyssnar <ailton.salg@gmail.com>
2020-10-18 20:31:59 -0500

    [ADD] prduct category internal_code

--- thikathani-mall/models/product_category.py
+++ thikathani-mall/models/product_category.py
@@ -3,4 +3,4 @@ from odoo import models, fields, api
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
-    internal_code = fields.char('Código de la categoría')
+    internal_code = fields.Char('Código de la categoría')
