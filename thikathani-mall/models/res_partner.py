5394d5be92a3c76fce00b9a96cecb3486cef2d2c
Kyssnar <ailton.salg@gmail.com>
2020-10-25 13:40:57 -0500

    address warehouse

--- thikathani-mall/models/res_partner.py
+++ thikathani-mall/models/res_partner.py
@@ -2,14 +2,10 @@
 from odoo import models, fields, api
 
 
-class Warehouse(models.Model):
-    _inherit = 'stock.warehouse'
+class Partner(models.Model):
+    _inherit = 'res.partner'
     # Supported
     #'partner_city': fields.related('res.partne', 'city', readonly=True, type='char', store=True)
     #city = fields.One2many('res.partner', 'partner_id', related="partner_id.city", readonly=True)
 
-    city = fields.Char('City', related='partner_id.city', readonly=True)
-    state = fields.Char('State', related='partner_id.state', readonly=True)
-    # state =
-    # district =
-    # image_url =
+    district = fields.Char('Distric')
