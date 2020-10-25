868a58e34cecd831f296854cd88923c124434b26
Kyssnar <ailton.salg@gmail.com>
2020-10-25 14:04:38 -0500

    address warehouse

--- thikathani-mall/models/stock_warehouse.py
+++ thikathani-mall/models/stock_warehouse.py
@@ -8,8 +8,8 @@ class Warehouse(models.Model):
     #'partner_city': fields.related('res.partne', 'city', readonly=True, type='char', store=True)
     #city = fields.One2many('res.partner', 'partner_id', related="partner_id.city", readonly=True)
 
-    partner_city = fields.Char('City', related='partner_id.city', readonly=True)
-    partner_state = fields.Char('State', related='partner_id.state', readonly=True)
+    #partner_city = fields.Char('City', related='partner_id.city', readonly=True)
+    #partner_state = fields.Char('State', related='partner_id.state', readonly=True)
     # state =
     # district =
     # image_url =
