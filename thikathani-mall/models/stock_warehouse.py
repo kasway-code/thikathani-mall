c5137cb614f67769b14bee4cb5e57148c18225c5
Kyssnar <ailton.salg@gmail.com>
2020-10-25 15:13:23 -0500

    address warehouse

--- thikathani-mall/models/stock_warehouse.py
+++ thikathani-mall/models/stock_warehouse.py
@@ -5,11 +5,13 @@ from odoo import models, fields, api
 class Warehouse(models.Model):
     _inherit = 'stock.warehouse'
     # Supported
-    #'partner_city': fields.related('res.partne', 'city', readonly=True, type='char', store=True)
+    # 'partner_city': fields.related('res.partne', 'city', readonly=True, type='char', store=True)
     #city = fields.One2many('res.partner', 'partner_id', related="partner_id.city", readonly=True)
 
     city = fields.Char('City', related='partner_id.city', readonly=True)
-    state = fields.Many2one('State', related='partner_id.state_id', readonly=True, store=True)
+    country = fields.Char(related='partner_id.state_id.country_id.name', string="Country")
+    #state = fields.Many2one(
+    #    'State', related='partner_id.state_id', readonly=True, store=True)
     #country = fields.Many2one('Country', related='partner_id.country_id', readonly=True, store=True)
     # district = fields.Char('District', related='partner_id.l10n_pe_district', readonly=True)
     # image_url =
