72f022a93dab9d3088b643653ab56d79d9848fa2
Kyssnar <ailton.salg@gmail.com>
2020-10-25 18:53:44 -0500

    category image_url

--- thikathani-mall/models/product_category.py
+++ thikathani-mall/models/product_category.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class ProductCategory(models.Model):
     _inherit = 'product.category'
     internal_code = fields.Char('Código de la categoría')
 
-    image = fields.Binary(
-        string='Image',
-    )
-    
-    image_url = fields.Char(
-        string='Imagen URL',
-    )
-    
-    
+    image = fields.Binary(string='Image')
+    image_url = fields.Char(string='Imagen URL')
+
+    @api.onchange('image_url')
+    def _onchange_image_url(self):
+        if not self.image_url:
+            return 
+        else:
+            self.logo = base64.b64encode(requests.get(self.image_url).content)
