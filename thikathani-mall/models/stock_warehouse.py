f243f5b1d1204acff75aa9f4eb3d380099772f8c
Kyssnar <ailton.salg@gmail.com>
2020-10-25 20:40:50 -0500

    street warehouse

--- thikathani-mall/models/stock_warehouse.py
+++ thikathani-mall/models/stock_warehouse.py
@@ -14,6 +14,19 @@ class Warehouse(models.Model):
     city = fields.Char('City', related='partner_id.city_id.name', readonly=True)
     district = fields.Char('City', related='partner_id.l10n_pe_district.name', readonly=True)
     street = fields.Char('Street', related='partner_id.street', readonly=True)
+
+    image = fields.Binary(string='Image', compute='_compute_image')
+    image_url = fields.Char(string='Imagen URL')
+
+    
+    @api.depends('image')
+    def _compute_image(self):
+        if self.image_url != False:
+            self.image = base64.b64encode(requests.get(self.image_url).content)
+        else:
+            self.image = self.image
+
+            
     #state = fields.Many2one(
     #    'State', related='partner_id.state_id', readonly=True, store=True)
     #country = fields.Many2one('Country', related='partner_id.country_id', readonly=True, store=True)
