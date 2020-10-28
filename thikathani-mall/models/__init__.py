bbb661f8d759f8fe10b81c9a96ad6fc45f3589c4
Kyssnar <ailton.salg@gmail.com>
2020-10-27 21:33:24 -0500

    change _name

--- thikathani-mall/models/__init__.py
+++ thikathani-mall/models/__init__.py
@@ -2,7 +2,7 @@
 
 from . import product_template
 from . import product_brand
-from . import product_type
+from . import product_property
 from . import product_category
 from . import stock_warehouse
 from . import res_partner
