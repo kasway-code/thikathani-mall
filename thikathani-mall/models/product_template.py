8aee98aa44b3f760968a72c1e3e9c488fc5ea155
Kyssnar <ailton.salg@gmail.com>
2020-10-27 22:47:29 -0500

    property product

--- thikathani-mall/models/product_template.py
+++ thikathani-mall/models/product_template.py
@@ -3,24 +3,23 @@ import requests
 
 from odoo import models, fields, api
 
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     descripcion_origen = fields.Text('Descripción y Origen')
     descripcion_beneficios = fields.Text('Beneficios')
     descripcion_usos = fields.Text('Modos de uso')
-    
-    brand_id = fields.Many2one('product.brand',string='Marca')
-    type_id = fields.Many2one('product.type',string='Tipo')
+
+    brand_id = fields.Many2one('product.brand', string='Marca')
+    type_id = fields.Many2one('product.type', string='Tipo')
     sku = fields.Char(string='SKU', compute='get_sku')
-    
-    
-    #property_ids = fields.One2many(
-    #    string='Propiedades',
-    #    comodel_name='product.property',
-    #    inverse_name='id',
-    #)
-    
+
+    property_ids = fields.One2many(
+        string='Propiedades',
+        comodel_name='product.property',
+        inverse_name='id',
+    )
 
     image_url = fields.Char(string='Imagen URL')
     image_1920 = fields.Binary(string='Image')
@@ -28,11 +27,11 @@ class ProductTemplate(models.Model):
     @api.depends('sku')
     def get_sku(self):
         self.sku = str(self.brand_id) + "test"
-        
+
     @api.onchange('image_url')
     def _onchange_image_url(self):
         if self.image_url != False and self.image_url != "":
-            self.image_1920 = base64.b64encode(requests.get(self.image_url).content)
+            self.image_1920 = base64.b64encode(
+                requests.get(self.image_url).content)
         else:
             self.image_1920 = False
-    
