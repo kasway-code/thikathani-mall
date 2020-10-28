3da7140efacc64a5d5779c941ce32c27e4aca150
Kyssnar <ailton.salg@gmail.com>
2020-10-27 20:19:46 -0500

    properties

--- thikathani-mall/models/product_property.py
+++ thikathani-mall/models/product_property.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api
 
 class ProductProperty(models.Model):
     _name = 'product.property'
+    property_image = fields.Binary("Image")
     #_inherit = 'product.category'
     
     # Cambiar a ProductProperty
