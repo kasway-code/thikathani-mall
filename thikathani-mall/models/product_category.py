77cded8d6c4c3579702def9fcb1e21e9766b3f58
Kyssnar <ailton.salg@gmail.com>
2020-10-27 20:14:13 -0500

    properties

--- thikathani-mall/models/product_category.py
+++ thikathani-mall/models/product_category.py
@@ -8,12 +8,12 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
     internal_code = fields.Char('Código de la categoría')
     #"test "
-    category_image = fields.Binary(string='Image')
+    image = fields.Binary(string='Image')
     image_url = fields.Char(string='Imagen URL')
 
     @api.onchange('image_url')
     def _onchage_image_url(self):
         if self.image_url != False and self.image_url != "":
-            self.category_image = base64.b64encode(requests.get(self.image_url).content)
+            self.image = base64.b64encode(requests.get(self.image_url).content)
         else:
-            self.category_image = False
+            self.image = False
