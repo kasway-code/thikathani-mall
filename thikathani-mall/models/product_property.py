d188511a23c6be5852b9f68dbc3c060e684080c3
Kyssnar <ailton.salg@gmail.com>
2020-10-27 21:47:15 -0500

    change _name

--- thikathani-mall/models/product_property.py
+++ thikathani-mall/models/product_property.py
@@ -1,5 +1,53 @@
 from odoo import models, fields, api
 
 class ProductProperty(models.Model):
-    _name = 'product.property'
-    name = fields.Char("Name")
+    _name = "product.property"
+    _description = "Product Property"
+    _parent_name = "parent_id"
+    _parent_store = True
+    _rec_name = 'complete_name'
+    _order = 'complete_name'
+
+    name = fields.Char('Name', index=True, required=True)
+    complete_name = fields.Char(
+        'Complete Name', compute='_compute_complete_name',
+        store=True)
+    parent_id = fields.Many2one('product.property', 'Parent Property', index=True, ondelete='cascade')
+    parent_path = fields.Char(index=True)
+    child_id = fields.One2many('product.property', 'parent_id', 'Child Categories')
+    product_count = fields.Integer(
+        '# Products', compute='_compute_product_count',
+        help="The number of products under this property (Does not consider the children categories)")
+
+    @api.depends('name', 'parent_id.complete_name')
+    def _compute_complete_name(self):
+        for property in self:
+            if property.parent_id:
+                property.complete_name = '%s / %s' % (property.parent_id.complete_name, property.name)
+            else:
+                property.complete_name = property.name
+
+    def _compute_product_count(self):
+        read_group_res = self.env['product.template'].read_group([('categ_id', 'child_of', self.ids)], ['categ_id'], ['categ_id'])
+        group_data = dict((data['categ_id'][0], data['categ_id_count']) for data in read_group_res)
+        for categ in self:
+            product_count = 0
+            for sub_categ_id in categ.search([('id', 'child_of', categ.ids)]).ids:
+                product_count += group_data.get(sub_categ_id, 0)
+            categ.product_count = product_count
+
+    @api.constrains('parent_id')
+    def _check_property_recursion(self):
+        if not self._check_recursion():
+            raise ValidationError(_('You cannot create recursive categories.'))
+        return True
+
+    @api.model
+    def name_create(self, name):
+        return self.create({'name': name}).name_get()[0]
+
+    def unlink(self):
+        main_property = self.env.ref('product.product_property_all')
+        if main_property in self:
+            raise UserError(_("You cannot delete this product property, it is the default generic property."))
+        return super().unlink()
