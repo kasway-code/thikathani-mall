7d34456a5f17f57718d8dd97459df19db964d9cd
Kyssnar <ailton.salg@gmail.com>
2020-10-27 19:27:08 -0500

    propertis

--- thikathani-mall/models/__init__.py
+++ thikathani-mall/models/__init__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
-
-from . import product_template
+from . import partner_product
 from . import product_brand
-from . import product_type
 from . import product_category
-from . import stock_warehouse
+from . import product_property
+from . import product_template
 from . import res_company
 from . import res_partner
+from . import stock_warehouse
