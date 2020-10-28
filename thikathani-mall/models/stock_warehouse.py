38fe66a04536164c8dee2bf1bf3027c3cca73c9b
Kyssnar <ailton.salg@gmail.com>
2020-10-27 20:52:39 -0500

    change _name

--- thikathani-mall/models/stock_warehouse.py
+++ thikathani-mall/models/stock_warehouse.py
@@ -6,23 +6,19 @@ from odoo import models, fields, api
 
 class Warehouse(models.Model):
     _inherit = 'stock.warehouse'
-    # Supported
-    # 'partner_city': fields.related('res.partne', 'city', readonly=True, type='char', store=True)
-    #city = fields.One2many('res.partner', 'partner_id', related="partner_id.city", readonly=True)
 
-    
-    country = fields.Char('Country', related='partner_id.state_id.country_id.name', readonly=True)
-    state = fields.Char('State', related='partner_id.state_id.name', readonly=True)
-    city = fields.Char('City', related='partner_id.city_id.name', readonly=True)
-    district = fields.Char('City', related='partner_id.l10n_pe_district.name', readonly=True)
-    street = fields.Char('Street', related='partner_id.street', readonly=True)
+    country_name = fields.Char('Country', related='partner_id.state_id.country_id.name', readonly=True)
+    state_name = fields.Char('State', related='partner_id.state_id.name', readonly=True)
+    city_name = fields.Char('City', related='partner_id.city_id.name', readonly=True)
+    district_name = fields.Char('City', related='partner_id.l10n_pe_district.name', readonly=True)
+    street_name = fields.Char('Street', related='partner_id.street', readonly=True)
 
     latitude = fields.Float('Latitud', related='partner_id.partner_latitude', readonly=True)
     longitude = fields.Float('Longitud', related='partner_id.partner_longitude', readonly=True)
 
     zip_code = fields.Char('Zip', related='partner_id.zip', readonly=True)
 
-    image = fields.Binary(string='Image')
+    warehouse_image = fields.Binary(string='Image')
     image_url = fields.Char(string='Imagen URL')
 
     
