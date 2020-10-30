756fa3ed4d6771b2686f3399e60ee37fece0f02b
Kyssnar <ailton.salg@gmail.com>
2020-10-29 20:23:59 -0500

    sale order product search read

--- thikathani-mall/models/sale_order.py
+++ thikathani-mall/models/sale_order.py
@@ -8,17 +8,23 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     numero_guia = fields.Char(string='Numero de guia')
-    #product_tmpl_list = fields.One2many(
-    #    string='Lista de productos', comodel_name='sale.order.line', inverse_name='order_id', compute='_compute_product_tmpl_list', store=True)
-    product_tmpl_list = []
-    # product_tmpl_list = fields.Char(
-    #    string='Lista de productos', compute='_compute_product_tmpl_list'
-    # )
+
+
+    product_tmpl_list = fields.One2many(
+       string='Lista de productos', comodel_name='sale.order.line', inverse_name='order_id', compute='_compute_product_tmpl_list', store=True)
+    
+    
+    #product_tmpl_list = []
+
+    product_tmpl_list = fields.Char(
+        string='Lista de productos', compute='_compute_product_tmpl_list')
 
     @api.onchange('numero_guia')
     def _compute_product_tmpl_list(self):
         for record in self:
-            record.product_tmpl_list = self.env['sale.order.line'].search_read([('order_id', '=', record.id)])
+            record.product_tmpl_list = str(self.env['sale.order.line'].search_read([('order_id', '=', record.id)]))
+
+
             '''
             record.product_tmpl_list = [
                 {
