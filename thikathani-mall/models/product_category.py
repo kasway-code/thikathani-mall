3faeac61acf0fe9d9fd6e3263e1f02a701702f47
Kyssnar <ailton.salg@gmail.com>
2020-11-02 18:55:50 -0500

    category image

--- thikathani-mall/models/product_category.py
+++ thikathani-mall/models/product_category.py
@@ -4,7 +4,7 @@ import requests
 from odoo import models, fields, api
 
 class ProductCategory(models.Model):
-    _inherits = ['product.category', 'image.mixin']
+    _inherit = ['product.category', 'image.mixin']
     internal_code = fields.Char('Código de la categoría')
 
     #category_image = fields.Binary(string='Image')
