77c24ce91728c1e870e9a07c5f369b8397231604
Kyssnar <ailton.salg@gmail.com>
2020-11-02 19:01:25 -0500

    category image

--- thikathani-mall/models/product_category.py
+++ thikathani-mall/models/product_category.py
@@ -4,7 +4,16 @@ import requests
 from odoo import models, fields, api
 
 class ProductCategory(models.Model):
-    _inherit = ['product.category', 'image.mixin']
+    _inherit = 'product.category'
+
+    image_1920 = fields.Image("Image", max_width=1920, max_height=1920)
+
+    # resized fields stored (as attachment) for performance
+    image_1024 = fields.Image("Image 1024", related="image_1920", max_width=1024, max_height=1024, store=True)
+    image_512 = fields.Image("Image 512", related="image_1920", max_width=512, max_height=512, store=True)
+    image_256 = fields.Image("Image 256", related="image_1920", max_width=256, max_height=256, store=True)
+    image_128 = fields.Image("Image 128", related="image_1920", max_width=128, max_height=128, store=True)
+
     internal_code = fields.Char('Código de la categoría')
 
     #category_image = fields.Binary(string='Image')
