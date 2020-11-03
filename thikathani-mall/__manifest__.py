2efc885e8d0b576a47f708034f242359deb31567
Kyssnar <ailton.salg@gmail.com>
2020-11-03 12:30:15 -0500

    hiding xml

--- thikathani-mall/__manifest__.py
+++ thikathani-mall/__manifest__.py
@@ -22,12 +22,6 @@
     ],
 
     'data': [
-        'views/product_brand_views.xml',
-        'views/product_property_views.xml',
-        'views/product_template_views.xml',
-        'views/product_category_views.xml',
-        'views/stock_warehouse_views.xml',
-        'views/product_value_attribute_value_views.xml',
         'security/ir.model.access.csv',
     ],
 
